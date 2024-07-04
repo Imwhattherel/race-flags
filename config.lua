0d877ccf914250a06a89372de2aff9da4b6fe2a9
WhatTheReL <79932956+Imwhattherel@users.noreply.github.com>
2024-07-04 18:26:40 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.JD_logs = false
+Config.JD_logsChannel = "flags"
+Config.JD_logsColor = "#F1F1F1"
+Config.Color = "~c~"
+
+-- Ace perm needed for staff commands
+Config.AcePerm = "jd.staff"
+
+
+
+-- Location for the  Status Display
+Config.DisplayX = 0.70
+Config.DisplayY = 0.075
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+author 'WhatTheRel'
+description 'Rel Race Flags'
+version '1.0.0'
+-- Config Script
+server_script 'config.lua'
+client_script 'config.lua'
+
+-- Server Scripts
+server_script 'server/main.lua'
+
+-- Client Scripts
+client_script 'client/main.lua'
+
+game 'gta5'
+fx_version 'bodacious'
